7a12682006fe7efc4f931b1cd8fdb635f9745b82
Milo Wical <milo_wical@carmax.com>
2021-01-04 07:23:10 -0500

    Slight refactor and added default DB files.

---
+++ src/mysql-test/README.txt
@@ -0,0 +1,15 @@
+Run the DB container from this directory with the following command:
+
+docker run --rm --name testdb -e MYSQL_ROOT_PASSWORD=password -d -p 3306:3306 -v $(pwd)/data:/var/lib/mysql mariadb
+
+Use this to stop it:
+
+docker stop testdb
+
+If you need to clear out the DB data, run this in this directory:
+
+rm -rf $(pwd)/data/*
+
+You may need to run the container and then stop it to force the 
+DB buffers to flush. The files will come around the next time the
+container is started.
--- src/mysql-test/src/main.rs
+++ src/mysql-test/src/main.rs
@@ -31,13 +31,12 @@ fn main() -> Result<()>{
   conn.query_drop(
     r"USE Test")?;
 
-  // Let's create a table for payments.
-  conn.query_drop(
-    r"CREATE TEMPORARY TABLE payment (
-      customer_id int not null,
-      amount int not null,
-      account_name text
-    )")?;
+  let table_count:Option<i32> = conn.query_first(r"SELECT COUNT(*) AS table_count
+  FROM information_schema.tables 
+  WHERE table_schema = 'Test'
+  AND table_name = 'payment';",)?;
+
+  println!("Table count: {}", table_count.unwrap());
 
   let payments = vec![
     Payment { customer_id: 1, amount: 2, account_name: None },
@@ -45,18 +44,12 @@ fn main() -> Result<()>{
     Payment { customer_id: 5, amount: 6, account_name: None },
     Payment { customer_id: 7, amount: 8, account_name: None },
     Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
-  ];  
+  ];
 
-  // Now let's insert payments to the database
-  conn.exec_batch(
-      r"INSERT INTO payment (customer_id, amount, account_name)
-        VALUES (:customer_id, :amount, :account_name)",
-      payments.iter().map(|p| params! {
-          "customer_id" => p.customer_id,
-          "amount" => p.amount,
-          "account_name" => &p.account_name,
-      })
-  )?;
+  // If the table doesn't exist, create it.
+  if table_count.unwrap() == 0 {
+    create_table(&mut conn, &payments);
+  }
 
   // Let's select payments from database. Type inference should do the trick here.
   let selected_payments = conn
@@ -67,13 +60,32 @@ fn main() -> Result<()>{
           },
       )?;
 
-      
-
   // Let's make sure, that `payments` equals to `selected_payments`.
   // Mysql gives no guaranties on order of returned rows
   // without `ORDER BY`, so assume we are lucky.
   assert_eq!(payments, selected_payments);
   println!("Yay!");
 
-  return Ok(());
+  Ok(())
+}
+
+fn create_table(conn: &mut PooledConn, payments: &Vec<Payment>) {
+  // Let's create a table for payments.
+  conn.query_drop(
+    r"CREATE TABLE payment (
+      customer_id int not null,
+      amount int not null,
+      account_name text
+    )").unwrap();  
+
+  // Now let's insert payments to the database
+  conn.exec_batch(
+      r"INSERT INTO payment (customer_id, amount, account_name)
+        VALUES (:customer_id, :amount, :account_name)",
+      payments.iter().map(|p| params! {
+          "customer_id" => p.customer_id,
+          "amount" => p.amount,
+          "account_name" => &p.account_name,
+      })
+  ).unwrap();
 }
