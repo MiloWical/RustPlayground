62feb6f466f68a5bdc8b3e080c82cd13e59b7c03
Milo Wical <milo_wical@carmax.com>
2021-08-18 17:16:24 -0400

    Shelving. Again.

--- src/actix-web-service/src/main.rs
+++ src/actix-web-service/src/main.rs
@@ -4,17 +4,12 @@ mod hello_world;
 mod shopping_cart;
 mod router;
 
-use shopping_cart::in_memory_cart_db::InMemoryCartDb;
-use shopping_cart::cart_db::CartDb;
 use router::route_config::routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            // .service(hello_world::hello)
-            // .service(hello_world::echo)
-            // .route("/hey", web::get().to(hello_world::manual_hello))
             .configure(routes)
     })
     .bind("127.0.0.1:8080")?
--- src/actix-web-service/src/router/route_config.rs
+++ src/actix-web-service/src/router/route_config.rs
@@ -1,9 +1,14 @@
-use actix_web::{get, post, HttpResponse, Responder};
+//use actix_web::{get, post, HttpResponse, Responder};
 use actix_web::web::{resource, get, head, ServiceConfig};
 
+use crate::hello_world::routes::*;
+
 pub fn routes(cfg: &mut ServiceConfig) {
-  cfg.service(resource("/test")
-      .route(get().to(|| HttpResponse::Ok()))
-      .route(head().to(|| HttpResponse::MethodNotAllowed()))
-  );
+  cfg
+    // .service(resource("/test")
+    // .route(get().to(|| HttpResponse::Ok()))
+    // .route(head().to(|| HttpResponse::MethodNotAllowed())));
+    .service(hello)
+    .service(echo)
+    .route("/hey", get().to(manual_hello));
 }
